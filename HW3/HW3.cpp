67cba78faae8d5ecfa82e91287841194a4368da1
Abner <abner@atlantico.com.br>
2020-07-27 13:14:29 -0300

    Save screenshot

--- HW3/HW3.cpp
+++ HW3/HW3.cpp
@@ -4,11 +4,29 @@
 #include "stdafx.h"
 
 #include <FreeImage.h>
+#include <iostream>
+#include <string>
+
+
+
+void saveScreenshot(BYTE * pixels, int w, int h, std::string fname)
+{
+	FIBITMAP *img = FreeImage_ConvertFromRawBits(pixels, w, h, w * 3, 24, 0xFF0000, 0x00FF00, 0x0000FF, true);
+
+	std::cout << "Saving screenshot: " << fname << "\n";
+
+	FreeImage_Save(FIF_PNG, img, fname.c_str(), 0);
+}
 
 
 int main()
 {
 	FreeImage_Initialise();
+	int w = 640, h = 480;
+	BYTE pixels[921600]; // 3 * w * h
+
+	saveScreenshot(pixels, w, h, "output.png");
+
 	FreeImage_DeInitialise();
     return 0;
 }
